12c69cfaf8e5729898a26ed352caada59e29902b
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-04 16:23:26 -0400

    added sass task and image copy

--- gruntfile.js
+++ gruntfile.js
@@ -2,19 +2,37 @@
 
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        copy: {
-            html: {
-                files: [
-                    { expand: true, cwd: 'src/', src: 'index.html', dest: 'build/' }
-                ]
-            }
+    copy: {
+      html: {
+        files: [
+          { expand: true, cwd: 'src/', src: 'index.html', dest: 'build/' }
+        ]
+      },
+      images: {
+        files: [
+          {
+            cwd:'src/images',
+            src:['*.jpg'],
+            dest:'build/images/',
+            expand: true
+          }
+        ]
+      }
+    },
+    sass: {
+      allSASS: {
+        files: {
+          'build/style.css':'src/sass/main.scss'
         }
+      }
+    }
 
-    });
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-    grunt.registerTask('default', [ 'copy' ]);
+  grunt.registerTask('default', [ 'copy', 'sass']);
 };
