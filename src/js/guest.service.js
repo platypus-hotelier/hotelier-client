5d9c2bd6373e04970bdff6eeed8e90d63403c0ab
Carlyse Jordan <carlyse.jordan@Carlyses-Air.lan>
2017-04-05 14:39:56 -0400

    created the guest service file and working on the guest controller

---
+++ src/js/guest.service.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+  angular.module('hotel').factory('GuestService', GuestService);
+
+  GuestService.$inject = ['$http'];
+  function GuestService($http){
+    /**
+     * getAllGuests should
+     * @return {Promise} 
+     */
+   function getAllGuests(){
+     return $http({
+       url: 'https://platypus-hotelier-api.herokuapp.com/api/guests',
+       method: 'Get'
+     })
+     .then(function handleResponse(response){
+       return response.data;
+     });
+
+
+   }
+   return {
+     getAllGuests: getAllStaff
+   };
+  }
+}());
