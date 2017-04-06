42e4290c6f4332dab4ebae40f4fa32c9028e3cab
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-06 08:47:30 -0400

    added angular copy task to build

--- gruntfile.js
+++ gruntfile.js
@@ -26,6 +26,16 @@ module.exports = function(grunt) {
             expand: true
           }
         ]
+      },
+      angular: {
+        files: [
+          {
+            cwd: 'node_modules/angular',
+            src: ['angular.js'],
+            dest: 'build/',
+            expand: true
+          }
+        ]
       }
     },
     sass: {
