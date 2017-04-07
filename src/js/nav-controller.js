4197bf341a2f12670b8eb44c54119517fa3c6f5d
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-07 16:57:11 -0400

    fixed indentation

--- src/js/nav-controller.js
+++ src/js/nav-controller.js
@@ -2,19 +2,19 @@
   'use strict';
 
   angular.module('hotel')
-    .controller('NavController', NavController);
+  .controller('NavController', NavController);
 
   NavController.$inject = ['StaffService'];
   function NavController(StaffService){
-      let vm = this;
+    let vm = this;
 
-      vm.logout = function logout(){
-        StaffService.logout();
-      };
+    vm.logout = function logout(){
+      StaffService.logout();
+    };
 
-      vm.isLoggedIn = function isLoggedIn(){
-        return !!StaffService.getToken();
-      };
+    vm.isLoggedIn = function isLoggedIn(){
+      return !!StaffService.getToken();
+    };
 
   }
 }());
