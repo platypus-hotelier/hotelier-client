b0cd66ffbc08ec554c82bdf1182a7b3b3de1d4f5
Sara Basile <sarabasile@Saras-Air.home>
2017-04-08 19:08:39 -0400

    added get all rooms function in room controller

--- src/js/room.controller.js
+++ src/js/room.controller.js
@@ -1,4 +1,28 @@
 (function() {
   'use strict';
-  
+
+  angular.module('hotel').controller('RoomController', RoomController);
+
+  RoomController.$inject = ['$state', 'RoomService'];
+
+  function RoomController($state, RoomService) {
+
+    let vm = this;
+
+    vm.rooms = [];
+    vm.room = {};
+
+    function getAllRooms() {
+      RoomService.getAllRooms()
+      .then(function handleRoomData(data) {
+        vm.rooms = data;
+      })
+      .catch(function handleError(err) {
+        console.warn(err);
+      });
+    }
+  }
+
+  vm.getAllRooms();
+
 }());
