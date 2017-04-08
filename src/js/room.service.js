976a2c7b09a7debec28c75ca438ece81a518926b
Sara Basile <sarabasile@Saras-Air.home>
2017-04-08 18:59:38 -0400

    added ajax call to get all rooms

--- src/js/room.service.js
+++ src/js/room.service.js
@@ -1,4 +1,27 @@
 (function() {
   'use strict';
-  
+
+
+  angular.module('hotel').factory('RoomService', RoomService);
+
+  RoomService.$inject = ['$http', 'StaffService'];
+
+  function RoomService($http, StaffService) {
+
+    function getAllRooms() {
+      return $http({
+        url: 'https://platypus-hotelier-api.herokuapp.com/api/Rooms',
+        method: 'GET',
+        headers: {
+          'Authorization': StaffService.getToken()
+        }
+      })
+      .then(function handleResponse(response) {
+        return response.data;
+      });
+    }
+    return {
+      getAllRooms: getAllRooms
+    };
+  }
 }());
